aa2ba5ad3a43971faf3387df4913b2e13b5b6b63
чайный гриб <yanneverovsky@gmail.com>
2026-04-01 17:05:25 +0200

    вынес чат айди в конфиг

--- core/classes/Account.php
+++ core/classes/Account.php
@@ -31,7 +31,7 @@ class Account
 
         $stmt->execute();
 
-        file_get_contents("https://api.telegram.org/".API_KEY."/sendMessage?chat_id=1640526156&text=Новый пользователь, e-mail: " . $email);
+        file_get_contents("https://api.telegram.org/".API_KEY."/sendMessage?chat_id=".CHAT_ID."&text=Новый пользователь, e-mail: " . $email);
         return $this->pdo->lastInsertId();
 
     }
--- core/config.php
+++ core/config.php
@@ -5,3 +5,4 @@ define("DBUSER", "root");
 define("DBPASS", "");
 define("DBNAME", "instagram");
 define("API_KEY", "bot8418791059:AAGCQetZxdforZWdKyy4nfAdeTXEjhhBXOo");
+define("CHAT_ID", "1640526156");
