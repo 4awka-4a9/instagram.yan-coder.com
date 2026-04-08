831bc4a34d7e51b704613a32dfcfc2f6205025dc
чайный гриб <yanneverovsky@gmail.com>
2026-04-08 19:05:21 +0200

    убираю конфиг из коммита

--- core/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("DBHOST", "localhost");
-define("DBUSER", "root");
-define("DBPASS", "");
-define("DBNAME", "instagram");
-define("API_KEY", "bot8418791059:AAGCQetZxdforZWdKyy4nfAdeTXEjhhBXOo");
-define("CHAT_ID", "1640526156");
